4c3f8e1f1a3536b47dbc9a4273328fe7fa8793f1
Pedro Marques <pedro.ivo996@gmail.com>
2022-03-24 10:34:24 +0000

    Folder Cleaning

--- Nuget Package Publisher/Nuget Package Publisher/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Nuget_Package_Publisher;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
